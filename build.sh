b77da14e5a875746ee765149a01bce83f7ae3a45
Pythux <pythux@nowhere.org>
2021-02-06 23:01:40 +0100

    add new build script with qmk cli

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+
+# qmk compile -kb <keyboard> -km <keymap>
+qmk compile -kb ergodox_ez -km pythux && \
+\
+echo "\nPress the RESET signal" && \
+qmk flash -kb ergodox_ez -km pythux
+# qmk flash use teensy loader
+
+# now, it's waiting to recev the RESET signal
